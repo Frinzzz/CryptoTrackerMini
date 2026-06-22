40300726fc73e96661968d7160209d22e6ae5e20
Francesco Pasini <francesco.pasini11@studio.unibo.it>
2026-06-22 13:46:27 +0200

    Initialize app dependency container in Application

--- app/src/main/java/com/example/cryptotrackermini/CryptoTrackerMiniApplication.kt
+++ app/src/main/java/com/example/cryptotrackermini/CryptoTrackerMiniApplication.kt
@@ -1,5 +1,14 @@
 package com.example.cryptotrackermini
 
 import android.app.Application
+import com.example.cryptotrackermini.di.AppContainer
 
-class CryptoTrackerMiniApplication : Application()
+class CryptoTrackerMiniApplication : Application() {
+    lateinit var appContainer: AppContainer
+        private set
+
+    override fun onCreate() {
+        super.onCreate()
+        appContainer = AppContainer(this)
+    }
+}
